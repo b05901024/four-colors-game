caf8d58b211775735e12e0726c4dc41bc3628f64
b05901024 <peikaiyeh@gmail.com>
2026-09-05 16:11:06 -0500

    Updates

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'four-colors-v2';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
